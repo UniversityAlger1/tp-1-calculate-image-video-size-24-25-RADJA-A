38b6987a39fcb7150d6c4573cab5e6e8122c7435
RADJA-A <radja.dahili@gmail.com>
2024-11-15 17:14:52 +0100

    TP1

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -7,14 +7,8 @@
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
    float value = 0;
-   value = w * h ;
+   value = (float)(w * h) ;
    // YOUR CODE HERE - END
    return (value);
 }
-/*
-int main(){
-   float test =0;
-   test = grayScaleImage(50,2.5);
-   printf("%.2f",test);
-   return 0 ;
-}*/
+
